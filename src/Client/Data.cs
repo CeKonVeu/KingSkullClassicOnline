98276423b8b0487ceba23188f3e286c75e635de4
Loic Rosset <loicrossetmp@gmail.com>
2022-09-02 09:24:57 +0200

    Amélioration de la solution

--- src/Client/Data.cs
+++ src/Client/Data.cs
@@ -4,6 +4,7 @@ namespace KingSkullClassicOnline.Client;
 
 public class Data
 {
+    public string? Name { get; set; }
     public static string? Player { get; set; }
     public static string? LobbyName { get; set; }
     public static HubConnection? HubConnection { get; set; }
