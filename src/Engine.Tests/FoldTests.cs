4dc581a07ea31c51717a6140ccf09b1bbcd74871
Loris199 <loris.marzullo@gmail.com>
2022-09-05 10:40:29 +0200

    Test TheFirstEscapePlayedWinIfThereAreOnlyEscapes

--- src/Engine.Tests/FoldTests.cs
+++ src/Engine.Tests/FoldTests.cs
@@ -8,14 +8,33 @@ namespace KingSkullClassicOnline.Engine.Tests;
 public class FoldTests
 {
     private Fold _fold;
-    private Player _p1, _p2;
-
+    private Player _p1, _p2, _p3;
+    
+    // TODO : Tester la ScaryMary
+    
+    // Tests d'égalités
+    
     [Test]
     public void TheFirstCardPlayedWinIfTheyHaveTheSameValue()
     {
         var c1 = Card.Pirate(1);
         var c2 = Card.Pirate(2);
         
+        _p1.Hand.Add(c1);
+        _p2.Hand.Add(c2);
+
+        _fold.PlayCard(_p1, _p1.Hand.First());
+        _fold.PlayCard(_p2, _p2.Hand.First());
+
+        Assert.AreEqual(_fold.GetWinner().Card, c1);
+    }
+    
+    [Test]
+    public void TheFirstEscapePlayedWinIfThereAreOnlyEscapes()
+    {
+        var c1 = Card.Escape();
+        var c2 = Card.Escape();
+
         _p1.Hand.Add(c1);
         _p2.Hand.Add(c2);
 
@@ -31,5 +50,6 @@ public class FoldTests
         _fold = new Fold();
         _p1 = new Player("1", "");
         _p2 = new Player("2", "");
+        _p3 = new Player("3", "");
     }
 }
