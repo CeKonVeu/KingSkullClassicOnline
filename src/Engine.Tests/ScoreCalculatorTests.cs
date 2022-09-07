fb427ee2e7052b779e86f165c9bba8782b20c539
Loris199 <loris.marzullo@gmail.com>
2022-09-07 15:39:36 +0200

    3ème joueur sur les tests votes > 1 et séparation des cas

--- src/Engine.Tests/ScoreCalculatorTests.cs
+++ src/Engine.Tests/ScoreCalculatorTests.cs
@@ -8,44 +8,52 @@ namespace KingSkullClassicOnline.Engine.Tests;
 public class ScoreCalculatorTests
 {
     private Fold[] _folds = null!;
-    private Player _p1 = null!, _p2 = null!;
-    private int _turn, _vote1, _vote2;
+    private Player _p1 = null!, _p2 = null!, _p3 = null!;
+    private int _turn, _vote1, _vote2, _vote3;
 
     /// <summary>
     ///     Défini les votes de 2 joueurs
     /// </summary>
     /// <param name="vote1">Vote de p1</param>
     /// <param name="vote2">Vote de p2</param>
-    private void SetAllVotes(int vote1, int vote2)
+    /// <param name="vote3">Vote de p3</param>
+    private void SetAllVotes(int vote1, int vote2, int vote3)
     {
         _vote1 = vote1;
         _vote2 = vote2;
+        _vote3 = vote3;
+        
         _p1.SetVote(_turn, _vote1);
         _p2.SetVote(_turn, _vote2);
+        _p3.SetVote(_turn, _vote3);
     }
 
     /// <summary>
-    ///     Permet de jouer 2 cartes durant un pli
+    ///     Permet de jouer 3 cartes durant un pli
     /// </summary>
     /// <param name="turn">Numéro du pli</param>
     /// <param name="c1">Carte de p1</param>
     /// <param name="c2">Carte de p2</param>
-    private void PlayFold(int turn, Card c1, Card c2)
+    /// <param name="c3">Carte de p3</param>
+    private void PlayFold(int turn, Card c1, Card c2, Card c3)
     {
         _folds[turn-1].PlayCard(_p1, c1);
         _folds[turn-1].PlayCard(_p2, c2);
+        _folds[turn-1].PlayCard(_p3, c3);
         
+        // Il est nécessaire de mettre à jour la valeur de Actual dans Vote
         var (winner, _) = _folds[turn-1].GetWinner();
         winner.AddActual(_turn);
     }
 
     /// <summary>
-    ///     Met à jour les scores des 2 joueurs
+    ///     Met à jour les scores des 3 joueurs
     /// </summary>
     private void UpdateAllScores()
     {
         ScoreCalculator.UpdateScore(_p1, _folds, _turn);
         ScoreCalculator.UpdateScore(_p2, _folds, _turn);
+        ScoreCalculator.UpdateScore(_p3, _folds, _turn);
     }
 
     /// <summary>
@@ -53,40 +61,78 @@ public class ScoreCalculatorTests
     /// </summary>
     /// <param name="score1">Score attendu de p1</param>
     /// <param name="score2">Score attendu de p2</param>
-    private void CheckScores(int score1, int score2)
+    /// <param name="score3">Score attendu de p3</param>
+    private void CheckScores(int score1, int score2, int score3)
     {
         Assert.AreEqual(_p1.GetTotal(_turn), score1);
         Assert.AreEqual(_p2.GetTotal(_turn), score2);
+        Assert.AreEqual(_p2.GetTotal(_turn), score3);
     }
     
-    private int GetWinVoteNot0(int vote)
+    private int GetGoodVoteNot0(int vote)
     {
         return Config.ScoreVoted * vote;
     }
 
-    private int GetLoseVoteNot0(int vote, Player p)
+    private int GetBadVoteNot0(int vote, Player p)
     {
         return Config.ScoreBadVote * Math.Abs(vote - p.GetVote(_turn)!.Actual);
     }
 
-    private int GetVote0(bool win)
+    private int GetGoodVote0()
     {
-        return Config.Score0 * _turn * (win ? 1 : -1);
+        return Config.Score0 * _turn;
+    }
+    
+    private int GetBadVote0()
+    {
+        return -(Config.Score0 * _turn);
     }
 
     // Test sur des votes > 1 //
     
+    /// <summary>
+    /// P1 gagne 2 plis sur 2 pariés
+    /// </summary>
     [Test]
-    public void ItShouldAddAndSubtractScoreCorrectlyWhenTheVoteIsNot0()
+    public void ItShouldAddScoreCorrectlyWhenTheVoteIsNot0AndCorrect()
     {
-        SetAllVotes(2, 2);
-        PlayFold(1, Card.NumberedCard(1, Color.Yellow), Card.Escape());
-        PlayFold(2, Card.NumberedCard(1, Color.Yellow), Card.Escape());
-        PlayFold(3, Card.Escape(), Card.NumberedCard(1, Color.Yellow));
+        SetAllVotes(2, 0, 0);
+        PlayFold(1, Card.NumberedCard(1, Color.Yellow), Card.Escape(), Card.Escape());
+        PlayFold(2, Card.NumberedCard(1, Color.Yellow), Card.Escape(), Card.Escape());
+        PlayFold(3, Card.Escape(), Card.NumberedCard(1, Color.Yellow), Card.Escape());
         UpdateAllScores();
-        CheckScores(GetWinVoteNot0(_vote1), GetLoseVoteNot0(_vote2, _p2));
+        Assert.AreEqual(_p1.GetTotal(_turn), GetGoodVoteNot0(_vote1));
     }
     
+    /// <summary>
+    /// P1 gagne 1 pli sur 2 pariés
+    /// </summary>
+    [Test]
+    public void ItShouldSubtractScoreCorrectlyWhenTheVoteIsNot0AndOvervalued()
+    {
+        SetAllVotes(2, 0, 0);
+        PlayFold(1, Card.NumberedCard(1, Color.Yellow), Card.Escape(), Card.Escape());
+        PlayFold(2, Card.Escape(), Card.NumberedCard(1, Color.Yellow), Card.Escape());
+        PlayFold(3, Card.Escape(), Card.NumberedCard(1, Color.Yellow), Card.Escape());
+        UpdateAllScores();
+        Assert.AreEqual(_p1.GetTotal(_turn), GetBadVoteNot0(_vote1, _p1));
+    }
+    
+    /// <summary>
+    /// P1 gagne 3 plis sur 2 pariés
+    /// </summary>
+    [Test]
+    public void ItShouldAddScoreCorrectlyWhenTheVoteIsCorrectAndNot0()
+    {
+        SetAllVotes(2, 0, 0);
+        PlayFold(1, Card.NumberedCard(1, Color.Yellow), Card.Escape(), Card.Escape());
+        PlayFold(2, Card.NumberedCard(1, Color.Yellow), Card.Escape(), Card.Escape());
+        PlayFold(3, Card.NumberedCard(1, Color.Yellow), Card.Escape(), Card.Escape());
+        UpdateAllScores();
+        Assert.AreEqual(_p1.GetTotal(_turn), GetBadVoteNot0(_vote1, _p1));
+    }
+/*
     // Test sur des votes = 0 //
 
     [Test]
@@ -110,7 +156,7 @@ public class ScoreCalculatorTests
         PlayFold(2, Card.Escape(), Card.Escape());
         PlayFold(3, Card.SkullKing(), Card.Mermaid());
         UpdateAllScores();
-        CheckScores(GetWinVoteNot0(_vote1) + Config.BonusMermaid, GetWinVoteNot0(_vote2) + Config.BonusMermaid);
+        CheckScores(GetGoodVoteNot0(_vote1) + Config.BonusMermaid, GetGoodVoteNot0(_vote2) + Config.BonusMermaid);
     }
     
     [Test]
@@ -121,7 +167,7 @@ public class ScoreCalculatorTests
         PlayFold(2, Card.Escape(), Card.Escape());
         PlayFold(3, Card.SkullKing(), Card.Mermaid());
         UpdateAllScores();
-        CheckScores(GetLoseVoteNot0(_vote1, _p1), GetLoseVoteNot0(_vote2, _p2));
+        CheckScores(GetBadVoteNot0(_vote1, _p1), GetBadVoteNot0(_vote2, _p2));
     }
     
     // Test des points bonus Skull King //
@@ -134,7 +180,7 @@ public class ScoreCalculatorTests
         PlayFold(2, Card.Escape(), Card.Escape());
         PlayFold(3, Card.Pirate(), Card.SkullKing());
         UpdateAllScores();
-        CheckScores(GetWinVoteNot0(_vote1) + Config.BonusSkullKing, GetWinVoteNot0(_vote2) + Config.BonusSkullKing);
+        CheckScores(GetGoodVoteNot0(_vote1) + Config.BonusSkullKing, GetGoodVoteNot0(_vote2) + Config.BonusSkullKing);
     }
     
     [Test]
@@ -145,17 +191,18 @@ public class ScoreCalculatorTests
         PlayFold(2, Card.Escape(), Card.Escape());
         PlayFold(3, Card.Pirate(), Card.SkullKing());
         UpdateAllScores();
-        CheckScores(GetLoseVoteNot0(_vote1, _p1), GetLoseVoteNot0(_vote2, _p2));
+        CheckScores(GetBadVoteNot0(_vote1, _p1), GetBadVoteNot0(_vote2, _p2));
     }
-
+*/
     /// <summary>
-    /// 2 joueurs, tour 3.
+    /// Situation : 3 joueurs lors du 3ème tour.
     /// </summary>
     [SetUp]
     public void Setup()
     {
         _p1 = new Player("1", "");
         _p2 = new Player("2", "");
+        _p3 = new Player("3", "");
         
         _turn = 3;
         _folds = new[]
@@ -163,9 +210,13 @@ public class ScoreCalculatorTests
             new Fold(), new Fold(), new Fold()
         };
         
+        // Initialise les votes des tours 1 et 2 par défaut
         _p1.SetVote(1, 0);
         _p2.SetVote(1, 0);
+        _p3.SetVote(1, 0);
+        
         _p1.SetVote(2, 0);
         _p2.SetVote(2, 0);
+        _p3.SetVote(2, 0);
     }
 }
