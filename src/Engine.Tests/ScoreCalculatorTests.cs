6d5f908c017bb9f935d32c1924bef098424b24b7
Loris199 <loris.marzullo@gmail.com>
2022-09-07 11:13:03 +0200

    Ajout de SetAllVotes et mise à jour des autres méthodes de test

--- src/Engine.Tests/ScoreCalculatorTests.cs
+++ src/Engine.Tests/ScoreCalculatorTests.cs
@@ -12,18 +12,32 @@ public class ScoreCalculatorTests
     private int _turn, _vote1, _vote2;
 
     /// <summary>
-    ///     Permet de jouer le dernier pli de la manche 3
+    ///     Défini les votes de 2 joueurs
     /// </summary>
-    /// <param name="c1">Carte de p1</param>
-    /// <param name="c2">Carte de p2</param>
-    private void PlayLastFold(Card c1, Card c2)
+    /// <param name="vote1">Vote d</param>
+    /// <param name="vote2"></param>
+    private void SetAllVotes(int vote1, int vote2)
     {
-        _folds[2].PlayCard(_p1, c1);
-        _folds[2].PlayCard(_p2, c2);
+        _vote1 = vote1;
+        _vote2 = vote2;
+        _p1.SetVote(_turn, _vote1);
+        _p2.SetVote(_turn, _vote2);
     }
 
     /// <summary>
-    ///     Met à jour les scores de tous les joueurs
+    ///     Permet de jouer 2 cartes durant un pli
+    /// </summary>
+    /// <param name="turn">Numéro du pli</param>
+    /// <param name="c1">Carte de p1</param>
+    /// <param name="c2">Carte de p2</param>
+    private void PlayFold(int turn, Card c1, Card c2)
+    {
+        _folds[turn].PlayCard(_p1, c1);
+        _folds[turn].PlayCard(_p2, c2);
+    }
+
+    /// <summary>
+    ///     Met à jour les scores des 2 joueurs
     /// </summary>
     private void UpdateAllScores()
     {
@@ -47,7 +61,10 @@ public class ScoreCalculatorTests
     [Test]
     public void ItShouldAddAndSubtractScoreCorrectlyWhenVoteIsNot0()
     {
-        PlayLastFold(Card.NumberedCard(10, Color.Black), Card.NumberedCard(1, Color.Yellow));
+        SetAllVotes(3, 3);
+        PlayFold(0, Card.NumberedCard(10, Color.Black), Card.NumberedCard(1, Color.Yellow));
+        PlayFold(1, Card.NumberedCard(10, Color.Black), Card.NumberedCard(1, Color.Yellow));
+        PlayFold(2, Card.NumberedCard(10, Color.Black), Card.NumberedCard(1, Color.Yellow));
         UpdateAllScores();
         CheckScores(_turn * Config.ScoreVoted, Math.Abs(_vote2 - 0) * Config.ScoreBadVote);
     }
@@ -58,18 +75,12 @@ public class ScoreCalculatorTests
     [Test]
     public void ItShouldCountScoreCorrectlyWhen0()
     {
-        _vote1 = 0;
-        _vote2 = 0;
-
-        _folds[2].PlayCard(_p1, new NumberedCard(10, "a", Colors.Black));
-        _folds[2].PlayCard(_p2, new NumberedCard(0, "a", Colors.Black));
-        ScoreCalculator.UpdateScore(_p1, _folds, _vote1, _turn);
-        ScoreCalculator.UpdateScore(_p2, _folds, _vote2, _turn);
-
-        Assert.AreEqual(_p1.Votes[_turn - 1], (_vote1, -(_turn * Config.Score0)));
-        Assert.AreEqual(_p2.Votes[_turn - 1], (_vote2, _turn * Config.Score0));
+        PlayLastFold(Card.NumberedCard(10, Color.Black), Card.NumberedCard(1, Color.Yellow));
+        UpdateAllScores();
+        CheckScores(_turn * Config.ScoreVoted, Math.Abs(_vote2 - 0) * Config.ScoreBadVote);
     }
 
+/*
     [Test]
     public void ItShouldGiveBonusForMermaidOnSkullKing()
     {
@@ -117,7 +128,6 @@ public class ScoreCalculatorTests
 */
     /// <summary>
     /// 2 joueurs, tour 3.
-    /// A la fin des 2 premiers plis, p1 gagne 2/3 plis, p2 gagne 0/2 plis
     /// </summary>
     [SetUp]
     public void Setup()
@@ -126,21 +136,9 @@ public class ScoreCalculatorTests
         _p2 = new Player("2", "");
 
         _turn = 3;
-        _vote1 = 3;
-        _vote2 = 2;
-        
-        _p1.SetVote(_turn, _vote1);
-        _p2.SetVote(_turn, _vote2);
-        
         _folds = new[]
         {
             new Fold(), new Fold(), new Fold()
         };
-
-        _folds[0].PlayCard(_p1, Card.NumberedCard(10, Color.Black));
-        _folds[0].PlayCard(_p2, Card.NumberedCard(1, Color.Yellow));
-
-        _folds[1].PlayCard(_p1, Card.NumberedCard(10, Color.Black));
-        _folds[1].PlayCard(_p2, Card.NumberedCard(1, Color.Yellow));
     }
 }
